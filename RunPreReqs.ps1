a3339cd7abe0b5262dd3800a9197379f273b1c22
MattSmith-ValoremReply <153131890+MattSmith-ValoremReply@users.noreply.github.com>
2024-07-10 13:21:12 -0500

    Update and rename Install_Chrome.ps1 to RunPreReqs.ps1

--- RunPreReqs.ps1
+++ RunPreReqs.ps1
@@ -1,5 +1,11 @@
+# Install Chrome
+
 $chromeInstallerUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
 $installerPath = "C:\chrome_installer.exe"
 Invoke-WebRequest -Uri $chromeInstallerUrl -OutFile $installerPath
 Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Wait
 Remove-Item -Path $installerPath
+
+# Run Install Mso Service
+$installerPath = "C:\InstallMSOService\InstallMSOService.exe"
+Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Wait

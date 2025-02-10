554026675268df97982aed2beccc0c19bc6dc4a3
MattSmith-ValoremReply <153131890+MattSmith-ValoremReply@users.noreply.github.com>
2025-02-10 09:46:24 -0600

    Update RunPreReqs.ps1

--- RunPreReqs.ps1
+++ RunPreReqs.ps1
@@ -49,7 +49,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module SharePointPnPPowerShellOnline -Force
 Install-Module -Name AIPService -RequiredVersion 2.0.0.3 -Scope AllUsers -Force -AllowClobber
 Install-Module -Name MicrosoftTeams -RequiredVersion 4.0.0 -Scope AllUsers -Force -AllowClobber
-Install-Module -Name ExchangeOnlineManagement -RequiredVersion 3.2.0 -Scope AllUsers -Force -AllowClobber
+Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber -Scope AllUsers
 
 # Run Hydration Engine
 Start-Process "C:\HydrationEngine\ProvisioningWorkerBootstrap.exe"
